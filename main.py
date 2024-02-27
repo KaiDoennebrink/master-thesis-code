2fefc5d79562461928f45c05931a093cde309234
Kai Dönnebrink <kai@doennebrink-lh.de>
2024-02-27 18:44:06 +0100

    feat: Adds CLI commandas for preparing PTB-XL and pretext data and training the pretext task models (#5)
    * feat: Adds CLI for preparing pretext task data
    
    * feat: Adds CLI for training models on the pretext task
    
    * feat: Adds CLI for loading and preprocessing the PTB-XL data
    
    * feat: Updates README with information how to use the CLI

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ import typer
 from tensorflow import keras
 
 from deepal_for_ecg.data.load import PTBXLDataLoader
+from deepal_for_ecg.data.tranformation_recognition import TransformationRecognitionDataModule
+from deepal_for_ecg.models.classification_heads import simple_classification_head
+from deepal_for_ecg.models.inception_network import InceptionNetworkConfig, InceptionNetworkBuilder
 from deepal_for_ecg.strategies.initalize.pt4al import PreTextLossInitQueryStrategy
+from deepal_for_ecg.train.transformation_recognition import TransformationRecognitionTrainer
+from deepal_for_ecg.util import improve_gpu_capacity
+
+improve_gpu_capacity()
 
 app = typer.Typer()
 
@@ -13,6 +20,58 @@ app = typer.Typer()
 @app.command()
 def hello(name: str):
     print(f"Hello {name}")
+
+
+@app.command()
+def train_pretext_model(base_name: str = "PretextInception", num_models: int = 1):
+    """
+    Trains models on the pretext tasks.
+
+    Args:
+        base_name (str): The base name of the model that is used to store it.
+        num_models (int): The number of models that should be trained on the pretext tasks (default = 1).
+    """
+    # load data
+    data_module = TransformationRecognitionDataModule()
+
+    # prepare the model builder
+    config = InceptionNetworkConfig(create_classification_head=simple_classification_head,
+                                    num_classes=data_module.NUM_TRANSFORMATIONS, input_shape=(1000, 12))
+    builder = InceptionNetworkBuilder()
+
+    # train the models
+    for i in range(num_models):
+        model = builder.build_model(config)
+        trainer = TransformationRecognitionTrainer(model=model, model_name=f"{base_name}{i}")
+        trainer.fit(data_module.train_dataset, data_module.validation_dataset)
+
+
+@app.command()
+def prepare_data_ptbxl():
+    """
+    Loads, preprocesses, and saves the PTB-XL dataset.
+    """
+    data_loader = PTBXLDataLoader()
+    data_loader.load_data()
+    data_loader.save_data()
+
+
+@app.command()
+def prepare_data_for_pretext_task():
+    """
+    Prepares the losses for the PT4AL query strategy for the given pre-trained model.
+    """
+    # load saved PTB-XL data
+    loader = PTBXLDataLoader(load_saved_data=True)
+    loader.load_data()
+    signal_data = loader.X_train.astype(np.float32)
+
+    # delete the loader to free up memory
+    del loader
+
+    data_module = TransformationRecognitionDataModule()
+    data_module.generate_and_split_data(signal_data)
+    data_module.prepare_datasets()
 
 
 @app.command()
